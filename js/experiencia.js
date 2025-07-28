7259d8332a2498f8d284b4de6c8cd4e291f45284
JoseMalcaR <jmalca@utem.cl>
2025-07-28 09:50:56 -0400

    agregando mas informacion

--- js/experiencia.js
+++ js/experiencia.js
@@ -1,25 +1,37 @@
 // Datos de experiencia
 const experienciaData = {
     utemtx: {
-        puesto: "Infraestructure Lead",
+        puesto: "Infraestructure Lead & DevOps -",
         empresa: "@ Utem-Tx",
         fecha: "Marzo 2024 - Julio 2024",
         responsabilidades: [
-            "Deliver high-quality, robust production code for a diverse array of projects for clients including Harvard Business School, Everytown for Gun Safety, Pratt Institute, Koala Health, Vanderbilt University, The 19th News, and more",
-            "Work alongside creative directors to lead the research, development, and architecture of technical solutions to fulfill business requirements",
-            "Collaborate with designers, project managers, and other engineers to transform creative concepts into production realities for clients and stakeholders",
-            "Provide leadership within engineering department through close collaboration, knowledge shares, and mentorship"
+"En el proyecto Utemtx desarrollamos una aplicación móvil y una página web de trading. Me desempeñé como líder de infraestructura y DevOps, siendo responsable de:" ,
+"Administrar la infraestructura en Google Cloud Platform (GCP)." ,
+"Implementar y mantener un entorno de microservicios en Kubernetes, utilizando GitHub Actions (Workflows) para la automatización del despliegue. ",
+"Dockerizar los microservicios, gestionando sus imágenes a través de Artifactory Registry.",
+" Hostear la página web en Firebase Hosting.",
+" Diseñar e implementar el flujo CI/CD asegurando integraciones y despliegues automatizados y eficientes.",
+" Este rol me permitió aplicar buenas prácticas de DevOps y escalar la infraestructura de forma segura y automatizada para un producto en constante desarrollo."
         ]
     },
     bobia: {
-        puesto: "Desarrollador Backend/ Infrastructure Lead",
+        puesto: "Desarrollador Backend/ Infrastructure Lead - ",
         empresa: "@ Bob-Ia",
         fecha: "Agosto 2024 - Diciembre 2024",
         responsabilidades: [
-            "Developed and shipped highly interactive web applications for Apple Music using Ember.js",
-            "Built and shipped the Apple Music Extension within Facebook Messenger leveraging third-party and internal APIs",
-            "Architected and implemented the front-end of Apple Music's embeddable web player widget",
-            "Contributed extensively to MusicKit.js, Apple's JavaScript SDK"
+            "En Bob IA(Prueba de concepto / trabajo de titulo), un motor de busqueda para Sodimac Argentina, desempeñé múltiples roles clave en el desarrollo de la solución:",
+
+            "Lideré la infraestructura del proyecto utilizando Google Cloud Platform (GCP).",
+
+            "Desarrollé e implementé microservicios backend en Python, los cuales fueron dockerizados y desplegados en Kubernetes.",
+
+            "Automatización del flujo de integración y despliegue continuo (CI/CD) mediante GitHub Actions, con almacenamiento de imágenes en Artifactory Registry.",
+
+            "Hosteamos el frontend del proyecto en Firebase Hosting.",
+
+            "Implementé e integré un modelo de inteligencia artificial de la familia Gemini a través de Model Garden en Vertex AI, permitiendo capacidades avanzadas de procesamiento del lenguaje natural.",
+
+            "Esta experiencia me permitió combinar habilidades de desarrollo backend, DevOps e inteligencia artificial en un entorno cloud escalable y automatizado."
         ]
     },
     goplicity: {
@@ -27,10 +39,17 @@ const experienciaData = {
         empresa: "@ Goplicity Spa",
         fecha: "Febrero 2025 - Abril 2025",
         responsabilidades: [
-            "Worked with a team of three designers to build a marketing website and e-commerce platform for blistabloc, an ambitious startup originating from Northeastern",
-            "Helped solidify a brand direction for blistabloc that spans both packaging and web",
-            "Developed and maintained code standards and documentation for the ongoing project",
-            "Interfaced with clients on a weekly basis, providing technological expertise"
+            "Durante mi práctica profesional en Goplicity SpA, trabajé como desarrollador fullstack, contribuyendo tanto en el backend como en el frontend de la plataforma de la empresa:.",
+
+            "Desarrollé APIs REST en Java 18 con Spring Boot, trabajando en la lógica de negocio y manejo de datos.",
+
+            "Realicé mantenimiento y corrección de bugs en módulos del frontend desarrollados con AngularJS.",
+
+            "Utilicé herramientas como Wrike para la gestión de tareas, y Postman para pruebas de APIs.",
+
+            "Aprendí y apliqué nuevas tecnologías y buenas prácticas en un entorno profesional, colaborando con equipos multidisciplinarios.",
+
+            "Esta experiencia me permitió afianzar mis conocimientos en desarrollo backend, resolver problemas reales en producción y mejorar mi capacidad para trabajar con tecnologías modernas en proyectos reales."
         ]
     },
 };
